94d473c54e83d2b7ca1b26297a9dc6c3f132be62
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-01-06 10:16:00 -0300

    🚀 Subindo Exercicios

---
+++ 3_EXERCICIOS/1_exercicio_01/scritpt.js
@@ -0,0 +1,3 @@
+console.log("andrei aspas duplas")
+console.log('andrei aspas simples')
+console.log(`andrei template literals`)
---
+++ 3_EXERCICIOS/2_exercicio_02/scritpt.js
@@ -0,0 +1,3 @@
+console.log(5)
+console.log(5.6)
+console.log(5 + (7.5 * 7) / 2)
