60ddfb3a64bb5871117b64867223d4c891ee706b
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-02-20 15:08:19 -0300

    🚀 Finalizando Objetos e Arrays Iniciando Exercicios

--- 8_OBJETOS_E_ARRAYS/27_json_string/scripts.js
+++ 8_OBJETOS_E_ARRAYS/27_json_string/scripts.js
@@ -10,6 +10,19 @@ console.log(pessoa.porcas)
 
 console.log(porcas)
 
+console.log("--------------------------------")
+
+let pessoaTexto = JSON.stringify(pessoa)    //Virou String
+
+console.log(pessoaTexto)       
+console.log(pessoaTexto.nome)               //[UNDEFINED]   
+
+let pessoaJSON = JSON.parse(pessoaTexto)    //Virou Objeto
+
+console.log(pessoaJSON)        
+
+
+
 //Mais Rigoroso
 //Apenas Aspas Duplas
 //Aceita comentarios
