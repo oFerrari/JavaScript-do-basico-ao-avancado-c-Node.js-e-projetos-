cf50fe10772e150651c14f82fd4c3f6d9028aa96
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-03-10 17:24:35 -0300

    ✨ Finalizando aula 1 e 2 ER

--- 13_EXPRESSOES_REGULARES/01_regex_test/scripts.js
+++ 13_EXPRESSOES_REGULARES/01_regex_test/scripts.js
@@ -1,15 +1,17 @@
-const reg1 = new RegExp('bola');
+const reg1 = new RegExp('arma');
 
-console.log(reg1.test("Tem bola?"));
-console.log(reg1.test("Não tem"));
+console.log(reg1.test("Tem arma?"));
+console.log(reg1.test("Não tem arma"));
+console.log(("--------------"));
 
 const reg2 = /bola/;
 
 let text = 'Tem bola na cesta';
 
 console.log(reg2.test("Tem bola?"));
-console.log(reg2.test("Não tem"));
+console.log(reg2.test("Não tem bol"));
 console.log(reg2.test(text));
+console.log(("--------------"));
 
 console.log(/quadrado/.test("Onde tem um quadrado"));
 console.log(/quadrado/.test("3423442323434quadrado2342323424332"));
--- 13_EXPRESSOES_REGULARES/02_conjuntos/scripts.js
+++ 13_EXPRESSOES_REGULARES/02_conjuntos/scripts.js
@@ -4,8 +4,17 @@ console.log(reg1.test("Temos o número 6"));
 console.log(reg1.test("Temos o número 2"));
 console.log(reg1.test("Temos o número 23"));
 console.log(reg1.test("Temos o número 60"));
+console.log(reg1.test("Temos o número 12345"));
+console.log(("--------------"));
 
 const reg2 = /[0-9]/;
 
-console.log(reg1.test("Temos o número 65448484884848484"));
-console.log(reg1.test("Temos o número"));
+console.log(reg2.test("Temos o número 65448484884848484"));
+console.log(reg2.test("Temos o número"));
+console.log(("--------------"));
+
+const reg3 = /8080/
+
+console.log(reg3.test("Temos a porta 8080"));
+console.log(reg3.test("Temos a porta 3000"));
+console.log(reg3.test("Temos a porta 3"));
