8e11a7678f7c5b506c5386ee39d41faffbaf034e
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2023-03-15 17:00:24 -0300

    🚀 Salvando Progresso Aula de Promisses

---
+++ 15_PROG_ASSINCRONA/01_callback/scripts.js
@@ -0,0 +1,8 @@
+console.log("Ola estou aqui agora");
+
+setTimeout(()=>{console.log("Ola estou aqui denovo")},5000)
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
---
+++ 15_PROG_ASSINCRONA/02_promise/scripts.js
@@ -0,0 +1,52 @@
+//Criação da Promessa
+
+const myPromise = new Promise((resolve,regect) => {
+    const nome = "Andrei"
+    
+    if(nome === "Andrei"){
+        resolve(`O ${nome} foi encontrado!!`)
+    }else{
+        regect(`O ${nome} nao foi encontrado`)
+    }
+})
+
+myPromise.then((dados) => {console.log(dados + "E agora estou feliz")})
+
+//Encadeamento do then
+
+
+const myPromise2 = new Promise((resolve,regect) => {
+    const nome = "Andrei"
+    
+    if(nome === "Andrei"){
+        resolve(`O ${nome} foi encontrado!!`)
+    }else{
+        regect(`O ${nome} nao foi encontrado`)
+    }
+})
+
+myPromise2.then((dados) => {
+    return dados.toUpperCase()
+}).then(function stringModified(stringModificada) {
+    console.log(stringModificada)
+})
+
+
+
+
+
+
+
+
+
+
+let promessa = Promise.resolve(5+5)
+
+console.log("Outros códigos");
+
+console.log(promessa);
+
+promessa.then((value) => { return value + 5})
+ .then((value) => {console.log(value)});
+
+ 
